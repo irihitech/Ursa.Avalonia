ca8e01ffa0860101dcee85c225e4d238d86f217b
rabbitism <popmessiah@hotmail.com>
2023-03-21 12:02:36 +0800

    feat: add resources

--- src/Ursa/Controls/Divider.cs
+++ src/Ursa/Controls/Divider.cs
@@ -1,17 +1,26 @@
 using Avalonia;
-using Avalonia.Collections;
 using Avalonia.Controls;
-using Avalonia.Controls.Shapes;
+using Avalonia.Layout;
 
 namespace Ursa.Controls;
 
 public class Divider : ContentControl
 {
-    public static readonly StyledProperty<AvaloniaList<double>?> DashArrayProperty = Shape.StrokeDashArrayProperty.AddOwner<Divider>();
-
-    public AvaloniaList<double>? DashArray
+    static Divider()
     {
-        get => GetValue(DashArrayProperty);
-        set => SetValue(DashArrayProperty, value);
+        HorizontalContentAlignmentProperty.OverrideDefaultValue<Divider>(HorizontalAlignment.Center);
+    }
+
+    public Divider()
+    {
+        HorizontalContentAlignment = HorizontalAlignment.Center;
+    }
+    
+    public static readonly StyledProperty<Orientation> OrientationProperty = AvaloniaProperty.Register<Divider, Orientation>(
+            nameof(Orientation));
+    public Orientation Orientation
+    {
+        get => GetValue(OrientationProperty);
+        set => SetValue(OrientationProperty, value);
     }
 }
