a19bbe118fa45fcef4e85ddecf0f53b9b403f347
rabbitism <popmessiah@hotmail.com>
2023-04-24 22:54:49 +0800

    feat: update demo.

--- demo/Ursa.Demo/ViewModels/TimelineDemoViewModel.cs
+++ demo/Ursa.Demo/ViewModels/TimelineDemoViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Ursa.Controls;
 
 namespace Ursa.Demo.ViewModels;
 
@@ -12,21 +13,24 @@ public class TimelineDemoViewModel: ObservableObject
             Time = DateTime.Now,
             TimeFormat = "yyyy-MM-dd HH:mm:ss",
             Description = "Item 1",
-            Content = "First"
+            Content = "First",
+            ItemType = TimelineItemType.Success,
         },
         new()
         {
             Time = DateTime.Now,
             TimeFormat = "HH:mm:ss",
             Description = "Item 2",
-            Content = "Content 2"
+            Content = "Content 2",
+            ItemType = TimelineItemType.Success,
         },
         new()
         {
             Time = DateTime.Now,
             TimeFormat = "HH:mm:ss",
             Description = "Item 3",
-            Content = "Content 3"
+            Content = "Content 3",
+            ItemType = TimelineItemType.Ongoing,
         },
         new()
         {
@@ -107,4 +111,5 @@ public class TimelineItemViewModel: ObservableObject
     public string? TimeFormat { get; set; }
     public string? Description { get; set; }    
     public string? Content { get; set; }
+    public TimelineItemType ItemType { get; set; }
 }
