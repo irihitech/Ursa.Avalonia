f606667a3f4d1c3c1a744f9945eadc9f380829ff
rabbitism <popmessiah@hotmail.com>
2023-06-23 12:54:27 +0800

    feat: add command

--- src/Ursa/Controls/Navigation/NavigationMenuItem.cs
+++ src/Ursa/Controls/Navigation/NavigationMenuItem.cs
@@ -1,4 +1,4 @@
-using System.Security.Cryptography.X509Certificates;
+using System.Windows.Input;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Metadata;
@@ -64,6 +64,24 @@ public class NavigationMenuItem: HeaderedSelectingItemsControl
     {
         get => _level;
         private set => SetAndRaise(LevelProperty, ref _level, value);
+    }
+
+    public static readonly StyledProperty<ICommand> CommandProperty = AvaloniaProperty.Register<NavigationMenuItem, ICommand>(
+        nameof(Command));
+
+    public ICommand Command
+    {
+        get => GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+
+    public static readonly StyledProperty<object?> CommandParameterProperty = AvaloniaProperty.Register<NavigationMenuItem, object?>(
+        nameof(CommandParameter));
+
+    public object? CommandParameter
+    {
+        get => GetValue(CommandParameterProperty);
+        set => SetValue(CommandParameterProperty, value);
     }
 
     static NavigationMenuItem()
@@ -139,7 +157,7 @@ public class NavigationMenuItem: HeaderedSelectingItemsControl
             this.PseudoClasses.Set(PC_Collapsed, _isCollapsed);
         }
         e.Handled = true;
-        
+        Command?.Execute(CommandParameter);
     }
 
     internal void SetSelection(NavigationMenuItem? source, bool selected, bool propagateToParent = false)
