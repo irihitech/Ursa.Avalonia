5672b2f1552275d401fbdef8bf6cb6057994a93d
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2023-12-08 23:47:51 +0800

    feat: intro page.

--- demo/Ursa.Demo/ViewModels/IntroductionDemoViewModel.cs
+++ demo/Ursa.Demo/ViewModels/IntroductionDemoViewModel.cs
@@ -3,9 +3,8 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Ursa.Demo.ViewModels;
 
-public class IntroductionDemoViewModel: ObservableObject
+public class IntroductionDemoViewModel : ObservableObject
 {
-    public string Greeting => "hello Intro";
     public ObservableCollection<MenuItemViewModel> MenuItems { get; set; } = new()
     {
         new MenuItemViewModel()
@@ -14,15 +13,17 @@ public class IntroductionDemoViewModel: ObservableObject
             MenuIconName = "User",
             Children = new ObservableCollection<MenuItemViewModel>()
             {
-                new (){
-                    MenuHeader = "公告管理" , 
+                new()
+                {
+                    MenuHeader = "公告管理",
                     MenuIconName = "Star",
                     Children = new ObservableCollection<MenuItemViewModel>()
                     {
-                        new () {MenuHeader = "公告设置"},
-                        new () {MenuHeader = "公告处理"}
-                    }},
-                new (){MenuHeader = "任务查询"}
+                        new() { MenuHeader = "公告设置" },
+                        new() { MenuHeader = "公告处理" }
+                    }
+                },
+                new() { MenuHeader = "任务查询" }
             }
         },
         new MenuItemViewModel()
@@ -36,9 +37,11 @@ public class IntroductionDemoViewModel: ObservableObject
             MenuIconName = "Gear",
             Children = new ObservableCollection<MenuItemViewModel>()
             {
-                new (){MenuHeader = "任务管理"},
-                new (){MenuHeader = "用户任务查询"}
+                new() { MenuHeader = "任务管理" },
+                new() { MenuHeader = "用户任务查询" }
             }
         }
     };
+
+    public ObservableCollection<string> ButtonGroupItems { get; set; } = ["Ding", "Otter", "Husky", "Mr. 17", "Cass"];
 }
