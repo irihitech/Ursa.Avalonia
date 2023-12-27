a5505139254fa3a94d2575f2f13ae63225a710a1
rabbitism <popmessiah@hotmail.com>
2023-12-27 17:38:42 +0800

    feat: add loading.

--- src/Ursa/Controls/Skeleton.cs
+++ src/Ursa/Controls/Skeleton.cs
@@ -8,13 +8,22 @@ namespace Ursa.Controls
 {
     public class Skeleton : ContentControl
     { 
-
+        
         public static readonly StyledProperty<bool> IsActiveProperty =
             AvaloniaProperty.Register<Skeleton, bool>(nameof(IsActive)); 
         public bool IsActive
         {
             get { return GetValue(IsActiveProperty); }
             set { SetValue(IsActiveProperty, value); }
-        } 
+        }
+
+        public static readonly StyledProperty<bool> LoadingProperty = 
+            AvaloniaProperty.Register<Skeleton, bool>(nameof(Loading));
+
+        public bool Loading
+        {
+            get => GetValue(LoadingProperty);
+            set => SetValue(LoadingProperty, value);
+        }
     }
 }
