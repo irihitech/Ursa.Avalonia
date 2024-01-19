28a8df285f94271c23f49585d95741e0420c5599
rabbitism <dongbin@irihi.tech>
2024-01-20 01:36:02 +0800

    fix: fix reapplying template initialization issue.

--- demo/Ursa.Demo/ViewModels/RangeSliderDemoViewModel.cs
+++ demo/Ursa.Demo/ViewModels/RangeSliderDemoViewModel.cs
@@ -1,8 +1,16 @@
+using System.Collections.ObjectModel;
+using Avalonia.Layout;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Ursa.Demo.ViewModels;
 
-public class RangeSliderDemoViewModel: ObservableObject
+public partial class RangeSliderDemoViewModel: ObservableObject
 {
-    
+    public ObservableCollection<Orientation> Orientations { get; set; } = new ObservableCollection<Orientation>()
+    {
+        Orientation.Horizontal,
+        Orientation.Vertical
+    };
+
+    [ObservableProperty] private Orientation _orientation;
 }
