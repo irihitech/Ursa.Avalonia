af02b46adc3584ce640a0b25d819f2a57a3ccf11
rabbitism <popmessiah@hotmail.com>
2024-01-25 17:35:52 +0800

    feat: update demo.

--- demo/Ursa.Demo/Dialogs/PlainDialogViewModel.cs
+++ demo/Ursa.Demo/Dialogs/PlainDialogViewModel.cs
@@ -1,8 +1,15 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Ursa.Demo.Dialogs;
 
 public class PlainDialogViewModel: ObservableObject
 {
-    
+    private DateTime? _date;
+
+    public DateTime? Date
+    {
+        get => _date;
+        set => SetProperty(ref _date, value);
+    }
 }
--- demo/Ursa.Demo/ViewModels/DialogDemoViewModel.cs
+++ demo/Ursa.Demo/ViewModels/DialogDemoViewModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
@@ -12,68 +13,140 @@ namespace Ursa.Demo.ViewModels;
 
 public class DialogDemoViewModel: ObservableObject
 {
-    public ICommand ShowLocalOverlayModalDialogCommand { get; }
-    public ICommand ShowGlobalOverlayModalDialogCommand { get; }
-    public ICommand ShowGlobalModalDialogCommand { get; }
-    public ICommand ShowGlobalOverlayDialogCommand { get; }
-    public ICommand ShowDefaultWindowCommand { get; }
+    public ICommand ShowDialogCommand { get; set; }
+    public ICommand ShowCustomDialogCommand { get; set; }
 
-    private object? _result;
-    public object? Result
+    private DialogMode _selectedMode;
+    public DialogMode SelectedMode
+    {
+        get => _selectedMode;
+        set => SetProperty(ref _selectedMode, value);
+    }
+    
+    public ObservableCollection<DialogMode> Modes { get; set; }
+    
+    private DialogButton _selectedButton;
+    public DialogButton SelectedButton
+    {
+        get => _selectedButton;
+        set => SetProperty(ref _selectedButton, value);
+    }
+    
+    public ObservableCollection<DialogButton> Buttons { get; set; }
+
+    private bool _isWindow;
+    public bool IsWindow
+    {
+        get => _isWindow;
+        set => SetProperty(ref _isWindow, value);
+    }
+    
+    private bool _isGlobal;
+    public bool IsGlobal
+    {
+        get => _isGlobal;
+        set => SetProperty(ref _isGlobal, value);
+    }
+
+    private bool _isModal;
+    public bool IsModal
+    {
+        get => _isModal;
+        set => SetProperty(ref _isModal, value);
+    }
+
+    private DialogResult? _defaultResult;
+    public DialogResult? DefaultResult
+    {
+        get => _defaultResult;
+        set => SetProperty(ref _defaultResult, value);
+    }
+
+    private bool _result;
+    public bool Result
     {
         get => _result;
         set => SetProperty(ref _result, value);
     }
 
-    private DateTime _date;
-
-    public DateTime Date
+    private DateTime? _date;
+    public DateTime? Date
     {
         get => _date;
         set => SetProperty(ref _date, value);
     }
-
-    public DialogWithActionViewModel DialogViewModel { get; set; } = new DialogWithActionViewModel();
-
+    
+    
     public DialogDemoViewModel()
     {
-        ShowLocalOverlayModalDialogCommand = new AsyncRelayCommand(ShowLocalOverlayModalDialog);
-        ShowGlobalOverlayModalDialogCommand = new AsyncRelayCommand(ShowGlobalOverlayModalDialog);
-        ShowGlobalModalDialogCommand = new AsyncRelayCommand(ShowGlobalModalDialog);
-        ShowGlobalOverlayDialogCommand = new RelayCommand(ShowGlobalOverlayDialog);
-        ShowDefaultWindowCommand = new AsyncRelayCommand(ShowDefaultWindow);
+        ShowDialogCommand = new AsyncRelayCommand(ShowDialog);
+        ShowCustomDialogCommand = new AsyncRelayCommand(ShowCustomDialog);
+        Modes = new ObservableCollection<DialogMode>(Enum.GetValues<DialogMode>());
+        Buttons = new ObservableCollection<DialogButton>(Enum.GetValues<DialogButton>());
     }
-
-    private async Task ShowDefaultWindow()
+    
+    private async Task ShowDialog()
     {
-        var result = await Dialog.ShowModalAsync<PlainDialog, PlainDialogViewModel>(new PlainDialogViewModel(),
-            mode: DialogMode.Error, buttons: DialogButton.OKCancel, title:"确定取消预约吗？");
-        Result = result;
-        return;
+        var vm = new PlainDialogViewModel();
+        if (IsWindow)
+        {
+            DefaultResult = await Dialog.ShowModalAsync<PlainDialog, PlainDialogViewModel>(
+                vm,
+                "Please select a date",
+                SelectedMode,
+                SelectedButton);
+            Date = vm.Date;
+        }
+        else
+        {
+            if (IsModal)
+            {
+                DefaultResult = await OverlayDialog.ShowModalAsync<PlainDialog, PlainDialogViewModel>(
+                    vm,
+                    IsGlobal ? null : "LocalHost",
+                    "Please select a date",
+                    SelectedMode,
+                    SelectedButton
+                );
+                Date = vm.Date;
+            }
+            else
+            {
+                OverlayDialog.Show<PlainDialog, PlainDialogViewModel>(
+                    new PlainDialogViewModel(),
+                    IsGlobal ? null : "LocalHost",
+                    "Please select a date",
+                    SelectedMode,
+                    SelectedButton);
+            }
+        }
+        
     }
-
-    private void ShowGlobalOverlayDialog()
-    {
-        OverlayDialog.ShowCustom<DialogWithAction, DialogWithActionViewModel>(new DialogWithActionViewModel());
-    }
-
-    private async Task ShowGlobalModalDialog()
-    {
-        var result = await Dialog.ShowCustomModalAsync<DialogWithAction, DialogWithActionViewModel, bool>(DialogViewModel);
-        Result = result;
-    }
-
-    private async Task ShowGlobalOverlayModalDialog()
-    {
-        Result = await OverlayDialog.ShowModalAsync<PlainDialog, PlainDialogViewModel>(new PlainDialogViewModel(),
-            title: "Please select a date", mode: DialogMode.Error, buttons: DialogButton.YesNoCancel);
-    }
-
-    private async Task ShowLocalOverlayModalDialog()
+    
+    private async Task ShowCustomDialog()
     {
         var vm = new DialogWithActionViewModel();
-        var result = await OverlayDialog.ShowCustomModalAsync<DialogWithAction, DialogWithActionViewModel, bool>(
-            vm, "LocalHost");
-        Result = result;
+        if (IsWindow)
+        {
+            
+            Result = await Dialog.ShowCustomModalAsync<DialogWithAction, DialogWithActionViewModel, bool>(
+                vm);
+            Date = vm.Date;
+        }
+        else
+        {
+            if (IsModal)
+            {
+                Result = await OverlayDialog.ShowCustomModalAsync<DialogWithAction, DialogWithActionViewModel, bool>(
+                    vm, IsGlobal ? null : "LocalHost");
+                Date = vm.Date;
+            }
+            else
+            {
+                OverlayDialog.ShowCustom<DialogWithAction, DialogWithActionViewModel>(new DialogWithActionViewModel(),
+                    IsGlobal ? null : "LocalHost");
+            }
+        }
+        
     }
 }
