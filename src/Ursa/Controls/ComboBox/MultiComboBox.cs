5d917d590546f11427083f1e9dc7a155b390f675
rabbitism <dongbin@irihi.tech>
2024-03-26 12:05:16 +0800

    feat: support clear.

--- src/Ursa/Controls/ComboBox/MultiComboBox.cs
+++ src/Ursa/Controls/ComboBox/MultiComboBox.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using Avalonia;
 using Avalonia.Collections;
@@ -9,14 +8,19 @@ using Avalonia.Controls.Primitives;
 using Avalonia.Controls.Templates;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
 using Irihi.Avalonia.Shared.Helpers;
 
 namespace Ursa.Controls;
 
 [TemplatePart(PART_BackgroundBorder, typeof(Border))]
+[PseudoClasses(PC_DropDownOpen, PC_Empty)]
 public class MultiComboBox: SelectingItemsControl
 {
     public const string PART_BackgroundBorder = "PART_BackgroundBorder";
+    public const string PC_DropDownOpen = ":dropdownopen";
+    public const string PC_Empty = ":selection-empty";
+    
     private Border? _rootBorder;
     
     private static ITemplate<Panel?> _defaultPanel = new FuncTemplate<Panel?>(() => new VirtualizingStackPanel());
@@ -52,6 +56,33 @@ public class MultiComboBox: SelectingItemsControl
     {
         FocusableProperty.OverrideDefaultValue<MultiComboBox>(true);
         ItemsPanelProperty.OverrideDefaultValue<MultiComboBox>(_defaultPanel);
+        IsDropDownOpenProperty.AffectsPseudoClass<MultiComboBox>(PC_DropDownOpen);
+        SelectedItemsProperty.Changed.AddClassHandler<MultiComboBox, IList?>((box, args) => box.OnSelectedItemsChanged(args));
+    }
+
+    public MultiComboBox()
+    {
+        if (SelectedItems is INotifyCollectionChanged c)
+        {
+            c.CollectionChanged+=OnSelectedItemsCollectionChanged;
+        }
+    }
+
+    private void OnSelectedItemsChanged(AvaloniaPropertyChangedEventArgs<IList?> args)
+    {
+        if (args.OldValue.Value is INotifyCollectionChanged old)
+        {
+            old.CollectionChanged-=OnSelectedItemsCollectionChanged;
+        }
+        if (args.NewValue.Value is INotifyCollectionChanged @new)
+        {
+            @new.CollectionChanged += OnSelectedItemsCollectionChanged;
+        }
+    }
+
+    private void OnSelectedItemsCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+    {
+        PseudoClasses.Set(PC_Empty, SelectedItems?.Count == 0);
     }
 
     protected override bool NeedsContainerOverride(object? item, int index, out object? recycleKey)
@@ -71,7 +102,7 @@ public class MultiComboBox: SelectingItemsControl
         PointerPressedEvent.RemoveHandler(OnBackgroundPointerPressed, _rootBorder);
         _rootBorder = e.NameScope.Find<Border>(PART_BackgroundBorder);
         PointerPressedEvent.AddHandler(OnBackgroundPointerPressed, _rootBorder);
-        
+        PseudoClasses.Set(PC_Empty, SelectedItems?.Count == 0);
     }
 
     private void OnBackgroundPointerPressed(object sender, PointerPressedEventArgs e)
@@ -104,5 +135,28 @@ public class MultiComboBox: SelectingItemsControl
             }
         }
         
+    }
+
+    public void Clear()
+    {
+        this.SelectedItems?.Clear();
+        var containers = Presenter?.Panel?.Children;
+        if(containers is null) return;
+        foreach (var container in containers)
+        {
+            if (container is MultiComboBoxItem t)
+            {
+                t.IsSelected = false;
+            }
+        }
+    }
+
+    protected override void OnUnloaded(RoutedEventArgs e)
+    {
+        base.OnUnloaded(e);
+        if (SelectedItems is INotifyCollectionChanged c)
+        {
+            c.CollectionChanged-=OnSelectedItemsCollectionChanged;
+        }
     }
 }
