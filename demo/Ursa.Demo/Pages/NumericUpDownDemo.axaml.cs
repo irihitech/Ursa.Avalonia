d9d58f83ff6409bece399bd04af714e5eb9a67ae
heartacker <heartacker@outlook.com>
2024-03-30 19:10:37 +0800

    update numeric updown demo

--- demo/Ursa.Demo/Pages/NumericUpDownDemo.axaml.cs
+++ demo/Ursa.Demo/Pages/NumericUpDownDemo.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using System;
 using System.Diagnostics;
@@ -10,16 +11,28 @@ namespace Ursa.Demo.Pages;
 
 public partial class NumericUpDownDemo : UserControl
 {
+    NumericUpDownDemoViewModel vm;
     public NumericUpDownDemo()
     {
         InitializeComponent();
-        DataContext = new NumericUpDownDemoViewModel();
+        DataContext = vm = new NumericUpDownDemoViewModel();
         numd.ValueChanged += Numd_ValueChanged;
+        numd.ReadRequested += Numd_ReadRequested;
+    }
+
+    Random random = new Random();
+    private void Numd_ReadRequested(object? sender, RoutedEventArgs e)
+    {
+        Trace.WriteLine(e.Source);
+        Trace.WriteLine(sender as NumericUIntUpDown);
+        var val = (sender as NumericUIntUpDown).Value;
+        var a = (uint)random.Next(0, 100);
+        vm.ReadRequestedUpdateText = $"ReadRequested,Old={val}, ChangeTo={a}";
+        vm.Value = a;
     }
 
     private void Numd_ValueChanged(object? sender, ValueChangedEventArgs<uint> e)
     {
-        Trace.WriteLine($"{(sender as NumericUIntUpDown).Name} {e.OldValue} {e.NewValue}");
-
+        vm.ValueChangedUpdateText = $"ValueChanged,Old={e.OldValue}, New={e.NewValue}";
     }
 }
