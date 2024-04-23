823bb2ee8c6489db8e94ccc2d2a3adfb821f48df
rabbitism <dongbin@irihi.tech>
2024-04-23 22:02:17 +0800

    feat: change hand length.

---
+++ src/Ursa.Themes.Semi/Converters/ClockHandLengthConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Ursa.Themes.Semi.Converters;
+
+public class ClockHandLengthConverter(double ratio) : IValueConverter
+{
+    public static ClockHandLengthConverter Hour { get; } = new(1-0.618);
+    public static ClockHandLengthConverter Minute { get; } = new(0.618);
+    public static ClockHandLengthConverter Second { get; } = new(1);
+    
+    private double _ratio = ratio;
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is double d)
+        {
+            return d * ratio / 2;
+        }
+        return 0.0;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
