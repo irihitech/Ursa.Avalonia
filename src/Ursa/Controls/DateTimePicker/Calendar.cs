98ec1d696ad50439150d8bd98dbaaa47cd65bfa4
rabbitism <dongbin@irihi.tech>
2024-05-03 23:58:54 +0800

    feat: add new control.

---
+++ src/Ursa/Controls/DateTimePicker/Calendar.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
+
+namespace Ursa.Controls;
+
+public class Calendar: TemplatedControl
+{
+    public static readonly StyledProperty<DateTime> SelectedDateProperty = AvaloniaProperty.Register<Calendar, DateTime>(nameof(SelectedDate), DateTime.Now);
+    public DateTime SelectedDate
+    {
+        get => GetValue(SelectedDateProperty);
+        set => SetValue(SelectedDateProperty, value);
+    }
+    
+    public static readonly StyledProperty<DayOfWeek> FirstDayOfWeekProperty = AvaloniaProperty.Register<Calendar, DayOfWeek>(nameof(FirstDayOfWeek), DayOfWeek.Sunday);
+    public DayOfWeek FirstDayOfWeek
+    {
+        get => GetValue(FirstDayOfWeekProperty);
+        set => SetValue(FirstDayOfWeekProperty, value);
+    }
+
+    public static readonly StyledProperty<bool> IsTodayHighlightedProperty = AvaloniaProperty.Register<Calendar, bool>(nameof(IsTodayHighlighted), true);
+    public bool IsTodayHighlighted
+    {
+        get => GetValue(IsTodayHighlightedProperty);
+        set => SetValue(IsTodayHighlightedProperty, value);
+    }
+}
