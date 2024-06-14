da25b0b3ffe8ab0da3d183b68bbf1fc5800d7d14
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2024-06-14 19:45:53 +0800

    feat: options.

--- demo/Ursa.Demo/ViewModels/ElasticWrapPanelDemoViewModel.cs
+++ demo/Ursa.Demo/ViewModels/ElasticWrapPanelDemoViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.ObjectModel;
-using Avalonia.Controls.Primitives;
+﻿using Avalonia.Controls.Primitives;
 using Avalonia.Layout;
 using CommunityToolkit.Mvvm.ComponentModel;
 
@@ -7,49 +6,19 @@ namespace Ursa.Demo.ViewModels;
 
 public partial class ElasticWrapPanelDemoViewModel : ObservableObject
 {
-    [ObservableProperty]
-    private ObservableCollection<Orientation> _orientations = [Orientation.Horizontal, Orientation.Vertical];
-
     [ObservableProperty] private Orientation _selectedOrientation = Orientation.Horizontal;
-
-    [ObservableProperty] private ObservableCollection<ScrollBarVisibility> _hScrollBarVisibilities =
-    [
-        ScrollBarVisibility.Disabled, ScrollBarVisibility.Auto, ScrollBarVisibility.Hidden, ScrollBarVisibility.Visible
-    ];
-
-    [ObservableProperty] private ObservableCollection<ScrollBarVisibility> _vScrollBarVisibilities =
-    [
-        ScrollBarVisibility.Disabled, ScrollBarVisibility.Auto, ScrollBarVisibility.Hidden, ScrollBarVisibility.Visible
-    ];
-
     [ObservableProperty] private ScrollBarVisibility _horizontalVisibility = ScrollBarVisibility.Auto;
     [ObservableProperty] private ScrollBarVisibility _verticalVisibility = ScrollBarVisibility.Auto;
 
-    [ObservableProperty] private bool _isFillHorizontal = true;
-    [ObservableProperty] private bool _isFillVertical = false;
+    [ObservableProperty] private bool _isFillHorizontal;
+    [ObservableProperty] private bool _isFillVertical;
     [ObservableProperty] private double _itemWidth = 40d;
     [ObservableProperty] private double _itemHeight = 40d;
 
-    [ObservableProperty] private bool _autoWidth;
-    [ObservableProperty] private bool _autoHeight;
+    [ObservableProperty] private bool _autoWidth = true;
+    [ObservableProperty] private bool _autoHeight = true;
     [ObservableProperty] private double _itemSelfWidth = double.NaN;
     [ObservableProperty] private double _itemSelfHeight = double.NaN;
-
-    [ObservableProperty] private ObservableCollection<HorizontalAlignment> _cmbHAligns =
-    [
-        HorizontalAlignment.Stretch,
-        HorizontalAlignment.Left,
-        HorizontalAlignment.Center,
-        HorizontalAlignment.Right
-    ];
-
-    [ObservableProperty] private ObservableCollection<VerticalAlignment> _cmbVAligns =
-    [
-        VerticalAlignment.Stretch,
-        VerticalAlignment.Top,
-        VerticalAlignment.Center,
-        VerticalAlignment.Bottom
-    ];
 
     [ObservableProperty] private HorizontalAlignment _cmbHAlign = HorizontalAlignment.Stretch;
     [ObservableProperty] private VerticalAlignment _cmbVAlign = VerticalAlignment.Stretch;
