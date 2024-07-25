a25e23c1d160ff3cdaf1e95e34bcaebea6f81b34
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-07-25 08:48:20 +0800

    add other dialog services demo;

--- demo/Ursa.PrismDialogDemo/MainWindow.axaml.cs
+++ demo/Ursa.PrismDialogDemo/MainWindow.axaml.cs
@@ -1,28 +1,48 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using DryIoc;
+using Ursa.Controls;
+using Ursa.Controls.Options;
 using Ursa.PrismExtension;
 
 namespace Ursa.PrismDialogDemo;
 
 public partial class MainWindow : Window
 {
-    private IUrsaOverlayDialogService _dialogService;
-    private IUrsaDrawerService _drawerService;
-    public MainWindow(IUrsaOverlayDialogService dialogService, IUrsaDrawerService drawerService)
+    private readonly IUrsaOverlayDialogService _overlayDialogService;
+    private readonly IUrsaDialogService _aloneDialogService;
+    private readonly IUrsaDrawerService _drawerService;
+
+    public MainWindow(IUrsaOverlayDialogService overlayDialogService, IUrsaDialogService aloneDialogService,
+        IUrsaDrawerService drawerService)
     {
         InitializeComponent();
-        _dialogService = dialogService;
+        _overlayDialogService = overlayDialogService;
+        _aloneDialogService = aloneDialogService;
         _drawerService = drawerService;
     }
 
-    private void DialogButton_OnClick(object? sender, RoutedEventArgs e)
+    private void OverlayDialogButton_OnClick(object? sender, RoutedEventArgs e)
     {
-        _dialogService.ShowModal("Default", null, null, null);
+        _overlayDialogService.ShowModal("Default", null, null, new OverlayDialogOptions()
+        {
+            Title = "This is dialog title"
+        });
+    }
+
+    private void AloneDialogButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        _aloneDialogService.ShowModal("Default", null, null, new DialogOptions()
+        {
+            Title = "This is dialog title"
+        });
     }
 
     private void DrawerButton_OnClick(object? sender, RoutedEventArgs e)
     {
-        _drawerService.ShowModal("Default", null, null, null);
+        _drawerService.ShowModal("Default", null, null, new DrawerOptions()
+        {
+            Title = "This is dialog title"
+        });
     }
 }
