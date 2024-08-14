b04f83f340d06d715982832ef9fc321d95970879
rabbitism <dongbin@irihi.tech>
2024-08-14 09:45:20 +0800

    feat: revert test window.

--- demo/Ursa.Demo/App.axaml.cs
+++ demo/Ursa.Demo/App.axaml.cs
@@ -18,9 +18,8 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new Window()
+            desktop.MainWindow = new MainWindow()
             {
-                Content = new SingleView(),
                 DataContext = new MainViewViewModel(),
             };
         }
