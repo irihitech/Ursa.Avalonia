65d13011dd3fccf64874ba24d0245909d82781cf
rabbitism <popmessiah@hotmail.com>
2024-08-29 17:36:10 +0800

    feat: explicitly assign empty string resource in code behind.

--- src/Ursa.Themes.Semi/Locale/en-us.axaml.cs
+++ src/Ursa.Themes.Semi/Locale/en-us.axaml.cs
@@ -1,8 +1,13 @@
 using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
 
 namespace Ursa.Themes.Semi.Locale;
 
 public class en_us: ResourceDictionary
 {
-    
+    public en_us()
+    {
+        AvaloniaXamlLoader.Load(this);
+        this["STRING_PAGINATION_PAGE"] = string.Empty;
+    }
 }
