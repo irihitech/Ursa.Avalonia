62a2f1d4cd1099df22213fec60105aee9994a939
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2024-09-13 14:41:39 +0800

    fix: fix navigation order in demo.

--- demo/Ursa.Demo/ViewModels/MenuViewModel.cs
+++ demo/Ursa.Demo/ViewModels/MenuViewModel.cs
@@ -2,7 +2,7 @@ using System.Collections.ObjectModel;
 
 namespace Ursa.Demo.ViewModels;
 
-public class MenuViewModel: ViewModelBase
+public class MenuViewModel : ViewModelBase
 {
     public ObservableCollection<MenuItemViewModel> MenuItems { get; set; }
 
@@ -13,21 +13,21 @@ public class MenuViewModel: ViewModelBase
             new() { MenuHeader = "Introduction", Key = MenuKeys.MenuKeyIntroduction, IsSeparator = false },
             new() { MenuHeader = "Controls", IsSeparator = true },
             new() { MenuHeader = "AutoCompleteBox", Key = MenuKeys.MenuKeyAutoCompleteBox, Status = "WIP" },
-            new() { MenuHeader = "Avatar", Key = MenuKeys.MenuKeyAvatar, Status = "WIP"},
+            new() { MenuHeader = "Avatar", Key = MenuKeys.MenuKeyAvatar, Status = "WIP" },
             new() { MenuHeader = "Badge", Key = MenuKeys.MenuKeyBadge },
             new() { MenuHeader = "Banner", Key = MenuKeys.MenuKeyBanner },
             new() { MenuHeader = "Breadcrumb", Key = MenuKeys.MenuKeyBreadcrumb },
             new() { MenuHeader = "Button Group", Key = MenuKeys.MenuKeyButtonGroup },
             new() { MenuHeader = "Class Input", Key = MenuKeys.MenuKeyClassInput },
             new() { MenuHeader = "Clock", Key = MenuKeys.MenuKeyClock, Status = "Updated" },
-            new() { MenuHeader = "Date Picker", Key = MenuKeys.MenuKeyDatePicker, Status = "New"},
-            new() { MenuHeader = "Date Time Picker", Key = MenuKeys.MenuKeyDateTimePicker, Status = "New"},
+            new() { MenuHeader = "Date Picker", Key = MenuKeys.MenuKeyDatePicker, Status = "New" },
+            new() { MenuHeader = "Date Time Picker", Key = MenuKeys.MenuKeyDateTimePicker, Status = "New" },
             new() { MenuHeader = "Dialog", Key = MenuKeys.MenuKeyDialog },
             new() { MenuHeader = "Disable Container", Key = MenuKeys.MenuKeyDisableContainer },
             new() { MenuHeader = "Divider", Key = MenuKeys.MenuKeyDivider },
             new() { MenuHeader = "Drawer", Key = MenuKeys.MenuKeyDrawer },
             new() { MenuHeader = "DualBadge", Key = MenuKeys.MenuKeyDualBadge },
-            new() { MenuHeader = "ElasticWrapPanel", Key = MenuKeys.MenuKeyElasticWrapPanel, Status = "New"},
+            new() { MenuHeader = "ElasticWrapPanel", Key = MenuKeys.MenuKeyElasticWrapPanel },
             new() { MenuHeader = "Enum Selector", Key = MenuKeys.MenuKeyEnumSelector },
             new() { MenuHeader = "Form", Key = MenuKeys.MenuKeyForm },
             new() { MenuHeader = "Icon Button", Key = MenuKeys.MenuKeyIconButton },
@@ -38,26 +38,26 @@ public class MenuViewModel: ViewModelBase
             new() { MenuHeader = "Message Box", Key = MenuKeys.MenuKeyMessageBox },
             new() { MenuHeader = "MultiComboBox", Key = MenuKeys.MenuKeyMultiComboBox, Status = "Updated" },
             new() { MenuHeader = "Nav Menu", Key = MenuKeys.MenuKeyNavMenu },
-            new() { MenuHeader = "Notification", Key = MenuKeys.MenuKeyNotification, Status = "New"},
-            new() { MenuHeader = "Number Displayer", Key = MenuKeys.MenuKeyNumberDisplayer, Status = "New" }, 
+            new() { MenuHeader = "Notification", Key = MenuKeys.MenuKeyNotification, Status = "New" },
+            new() { MenuHeader = "Number Displayer", Key = MenuKeys.MenuKeyNumberDisplayer, Status = "New" },
             new() { MenuHeader = "Numeric UpDown", Key = MenuKeys.MenuKeyNumericUpDown },
             new() { MenuHeader = "NumPad", Key = MenuKeys.MenuKeyNumPad },
             new() { MenuHeader = "Pagination", Key = MenuKeys.MenuKeyPagination },
-            new() { MenuHeader = "PinCode", Key = MenuKeys.MenuKeyPinCode},
+            new() { MenuHeader = "PinCode", Key = MenuKeys.MenuKeyPinCode },
             new() { MenuHeader = "RangeSlider", Key = MenuKeys.MenuKeyRangeSlider },
-            new() { MenuHeader = "Rating", Key = MenuKeys.MenuKeyRating, Status = "New"},
+            new() { MenuHeader = "Rating", Key = MenuKeys.MenuKeyRating },
             new() { MenuHeader = "Scroll To", Key = MenuKeys.MenuKeyScrollToButton },
             new() { MenuHeader = "Selection List", Key = MenuKeys.MenuKeySelectionList },
             new() { MenuHeader = "Skeleton", Key = MenuKeys.MenuKeySkeleton },
             new() { MenuHeader = "TagInput", Key = MenuKeys.MenuKeyTagInput, Status = "Updated" },
             new() { MenuHeader = "Theme Toggler", Key = MenuKeys.MenuKeyThemeToggler },
-            new() { MenuHeader = "TimePicker", Key = MenuKeys.MenuKeyTimePicker },
-            new() { MenuHeader = "Timeline", Key = MenuKeys.MenuKeyTimeline },
-            new() { MenuHeader = "TreeComboBox", Key = MenuKeys.MenuKeyTreeComboBox },
-            new() { MenuHeader = "TwoTonePathIcon", Key = MenuKeys.MenuKeyTwoTonePathIcon},
-            new() { MenuHeader = "Toast", Key = MenuKeys.MenuKeyToast, Status = "New"},
-            new() { MenuHeader = "ToolBar", Key = MenuKeys.MenuKeyToolBar },
             new() { MenuHeader = "Time Box", Key = MenuKeys.MenuKeyTimeBox },
+            new() { MenuHeader = "Timeline", Key = MenuKeys.MenuKeyTimeline },
+            new() { MenuHeader = "TimePicker", Key = MenuKeys.MenuKeyTimePicker },
+            new() { MenuHeader = "Toast", Key = MenuKeys.MenuKeyToast, Status = "New" },
+            new() { MenuHeader = "ToolBar", Key = MenuKeys.MenuKeyToolBar },
+            new() { MenuHeader = "TreeComboBox", Key = MenuKeys.MenuKeyTreeComboBox },
+            new() { MenuHeader = "TwoTonePathIcon", Key = MenuKeys.MenuKeyTwoTonePathIcon },
         };
     }
 }
@@ -69,23 +69,23 @@ public static class MenuKeys
     public const string MenuKeyAvatar = "Avatar";
     public const string MenuKeyBadge = "Badge";
     public const string MenuKeyBanner = "Banner";
+    public const string MenuKeyBreadcrumb = "Breadcrumb";
     public const string MenuKeyButtonGroup = "ButtonGroup";
-    public const string MenuKeyBreadcrumb= "Breadcrumb";
     public const string MenuKeyClassInput = "Class Input";
     public const string MenuKeyClock = "Clock";
     public const string MenuKeyDatePicker = "DatePicker";
     public const string MenuKeyDateTimePicker = "DateTimePicker";
     public const string MenuKeyDialog = "Dialog";
-    public const string MenuKeyDivider = "Divider";
     public const string MenuKeyDisableContainer = "DisableContainer";
+    public const string MenuKeyDivider = "Divider";
     public const string MenuKeyDrawer = "Drawer";
     public const string MenuKeyDualBadge = "DualBadge";
     public const string MenuKeyElasticWrapPanel = "ElasticWrapPanel";
     public const string MenuKeyEnumSelector = "EnumSelector";
     public const string MenuKeyForm = "Form";
+    public const string MenuKeyIconButton = "IconButton";
     public const string MenuKeyImageViewer = "ImageViewer";
     public const string MenuKeyIpBox = "IPv4Box";
-    public const string MenuKeyIconButton = "IconButton";
     public const string MenuKeyKeyGestureInput = "KeyGestureInput";
     public const string MenuKeyLoading = "Loading";
     public const string MenuKeyMessageBox = "MessageBox";
@@ -96,20 +96,19 @@ public static class MenuKeys
     public const string MenuKeyNumericUpDown = "NumericUpDown";
     public const string MenuKeyNumPad = "NumPad";
     public const string MenuKeyPagination = "Pagination";
+    public const string MenuKeyPinCode = "PinCode";
     public const string MenuKeyRangeSlider = "RangeSlider";
     public const string MenuKeyRating = "Rating";
     public const string MenuKeyScrollToButton = "ScrollToButton";
     public const string MenuKeySelectionList = "SelectionList";
+    public const string MenuKeySkeleton = "Skeleton";
     public const string MenuKeyTagInput = "TagInput";
-    public const string MenuKeySkeleton = "Skeleton"; 
-    public const string MenuKeyTimePicker = "TimePicker";
-    public const string MenuKeyTimeline = "Timeline";
-    public const string MenuKeyTwoTonePathIcon = "TwoTonePathIcon";
     public const string MenuKeyThemeToggler = "ThemeToggler";
-    public const string MenuKeyTreeComboBox = "TreeComboBox";
+    public const string MenuKeyTimeBox = "TimeBox";
+    public const string MenuKeyTimeline = "Timeline";
+    public const string MenuKeyTimePicker = "TimePicker";
     public const string MenuKeyToast = "Toast";
     public const string MenuKeyToolBar = "ToolBar";
-    public const string MenuKeyPinCode = "PinCode";
-    public const string MenuKeyTimeBox = "TimeBox";
-
+    public const string MenuKeyTreeComboBox = "TreeComboBox";
+    public const string MenuKeyTwoTonePathIcon = "TwoTonePathIcon";
 }
