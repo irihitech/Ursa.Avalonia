52e44f2f5d4c03a79623e71cc8da2e0459cff01e
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2024-09-14 14:04:09 +0800

    feat: add build.sh.

---
+++ build.sh
@@ -0,0 +1,3 @@
+dotnet build ./src/Ursa/Ursa.csproj -c Release
+dotnet build ./src/Ursa.Themes.Semi/Ursa.Themes.Semi.csproj -c Release
+dotnet build ./src/Ursa.PrismExtension/Ursa.PrismExtension.csproj -c Release
