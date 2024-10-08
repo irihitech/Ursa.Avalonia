b733621362fcfaf6573b868b3f59cc2ccfcb91eb
rabbitism <popmessiah@hotmail.com>
2024-10-08 22:46:38 +0800

    test: add bulk assignment test case.

--- tests/HeadlessTest.Ursa/Controls/BreadcrumbTests/BreadcrumbTests.cs
+++ tests/HeadlessTest.Ursa/Controls/BreadcrumbTests/BreadcrumbTests.cs
@@ -63,4 +63,25 @@ public class BreadcrumbTests
         Assert.Contains(":last", item2.Classes);
         Assert.DoesNotContain(":last", item1.Classes);
     }
+    
+    [AvaloniaFact]
+    public void Bulk_Added_BreadcrumbItem_Should_Have_Correct_PseudoClasses()
+    {
+        var window = new Window();
+        var breadcrumb = new Breadcrumb();
+        window.Content = breadcrumb;
+        window.Show();
+        var items = new ObservableCollection<string>
+        {
+            "Item 1",
+            "Item 2"
+        };
+        breadcrumb.ItemsSource = items;
+        var item1 = breadcrumb.GetLogicalChildren().OfType<BreadcrumbItem>().FirstOrDefault();
+        var item2 = breadcrumb.GetLogicalChildren().OfType<BreadcrumbItem>().LastOrDefault();
+        Assert.NotNull(item1);
+        Assert.NotNull(item2);
+        Assert.Contains(":last", item2.Classes);
+        Assert.DoesNotContain(":last", item1.Classes);
+    }
 }
