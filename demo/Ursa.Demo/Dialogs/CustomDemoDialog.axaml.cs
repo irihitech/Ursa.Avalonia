b16a51ee4f566d01b7dba56a3b8bebf8e92b03ed
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2024-12-23 17:22:02 +0800

    feat: add VisualLayerManager ctor to Notification & Toast manager.

--- demo/Ursa.Demo/Dialogs/CustomDemoDialog.axaml.cs
+++ demo/Ursa.Demo/Dialogs/CustomDemoDialog.axaml.cs
@@ -1,13 +1,36 @@
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.Controls.Primitives;
+using Avalonia.VisualTree;
+using Ursa.Controls;
 
 namespace Ursa.Demo.Dialogs;
 
 public partial class CustomDemoDialog : UserControl
 {
+    private CustomDemoDialogViewModel? _viewModel;
+
     public CustomDemoDialog()
     {
         InitializeComponent();
+    }
+
+    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnAttachedToVisualTree(e);
+        _viewModel = this.DataContext as CustomDemoDialogViewModel;
+        var visualLayerManager = this.FindAncestorOfType<VisualLayerManager>();
+        if (visualLayerManager is not null && _viewModel is not null)
+        {
+            _viewModel.NotificationManager = new WindowNotificationManager(visualLayerManager) { MaxItems = 3 };
+            _viewModel.ToastManager = new WindowToastManager(visualLayerManager) { MaxItems = 3 };
+        }
+    }
+
+    protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnDetachedFromVisualTree(e);
+        _viewModel?.NotificationManager?.Uninstall();
+        _viewModel?.ToastManager?.Uninstall();
     }
 }
