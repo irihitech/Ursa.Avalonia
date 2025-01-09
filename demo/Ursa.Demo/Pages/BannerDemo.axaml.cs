954ca442f538c71441b1fbcd68a4769b487813a6
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-01-09 18:22:40 +0800

    feat: redesign banner demo.

--- demo/Ursa.Demo/Pages/BannerDemo.axaml.cs
+++ demo/Ursa.Demo/Pages/BannerDemo.axaml.cs
@@ -1,7 +1,4 @@
 using Avalonia.Controls;
-using Avalonia.Controls.Notifications;
-using Avalonia.Markup.Xaml;
-using CommunityToolkit.Mvvm.ComponentModel;
 using Ursa.Demo.ViewModels;
 
 namespace Ursa.Demo.Pages;
@@ -13,16 +10,4 @@ public partial class BannerDemo : UserControl
         InitializeComponent();
         this.DataContext = new BannerDemoViewModel();
     }
-
-    private void InitializeComponent()
-    {
-        AvaloniaXamlLoader.Load(this);
-    }
-}
-
-public partial class BannerDemoViewModel : ViewModelBase
-{
-    [ObservableProperty] private NotificationType _selectedType;
-    [ObservableProperty] private bool _bordered;
-    [ObservableProperty] private bool _canClose;
 }
--- demo/Ursa.Demo/ViewModels/BannerDemoViewModel.cs
+++ demo/Ursa.Demo/ViewModels/BannerDemoViewModel.cs
@@ -1,6 +1,41 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace Ursa.Demo.ViewModels;
 
-public class BannerDemoViewModel: ViewModelBase
+public partial class BannerDemoViewModel : ViewModelBase
 {
-    
+    private string? _oldTitle = string.Empty;
+    private string? _oldContent = string.Empty;
+    [ObservableProperty] private string? _title = "Welcome to Ursa";
+    [ObservableProperty] private string? _content = "This is the Demo of Ursa Banner.";
+    [ObservableProperty] private bool _bordered;
+
+    [ObservableProperty] private bool _setTitleNull = true;
+    [ObservableProperty] private bool _setContentNull = true;
+
+    partial void OnSetTitleNullChanged(bool value)
+    {
+        if (value)
+        {
+            Title = _oldTitle;
+        }
+        else
+        {
+            _oldTitle = Title;
+            Title = null;
+        }
+    }
+
+    partial void OnSetContentNullChanged(bool value)
+    {
+        if (value)
+        {
+            Content = _oldContent;
+        }
+        else
+        {
+            _oldContent = Content;
+            Content = null;
+        }
+    }
 }
