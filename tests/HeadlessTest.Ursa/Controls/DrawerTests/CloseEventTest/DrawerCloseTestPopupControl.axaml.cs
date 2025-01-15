87060da654d518b4818c722f19a127a9472adfd7
望尘空忧 <wckfxxjstdwckyxphdhqxg@outlook.com>
2025-01-15 22:01:48 +0800

    Add:OverlayFeedbackElement.ClosedEvent fixed HeadlessTest.

---
+++ tests/HeadlessTest.Ursa/Controls/DrawerTests/CloseEventTest/DrawerCloseTestPopupControl.axaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Ursa.Controls;
+
+namespace HeadlessTest.Ursa.Controls;
+
+public partial class DrawerCloseTestPopupControl : UserControl
+{
+    public DrawerCloseTestPopupControl()
+    {
+        InitializeComponent();
+        _overlayDialogHost.HostId = _hostid;
+    }
+
+    private readonly string _hostid = Path.GetRandomFileName();
+    public DrawerCloseTestPopupControl Popup { get; set; }
+    public int LResult { get; set; }
+    public int RResult { get; set; }
+
+    public async void OpenPopup()
+    {
+        var vm = new DrawerCloseTestPopupControlVM();
+        LResult = vm.Result;
+        RResult = await Drawer.ShowCustomModal<int>(Popup = new(), vm, _hostid);
+    }
+
+    public void ClosePopup()
+    {
+        (Popup.DataContext as DrawerCloseTestPopupControlVM)?.Close();
+    }
+
+
+    public void Close()
+    {
+        (DataContext as DrawerCloseTestPopupControlVM)?.Close();
+    }
+}
---
+++ tests/HeadlessTest.Ursa/Controls/DrawerTests/CloseEventTest/DrawerCloseTestPopupControlVM.cs
@@ -0,0 +1,15 @@
+﻿using Irihi.Avalonia.Shared.Contracts;
+
+namespace HeadlessTest.Ursa.Controls;
+
+public class DrawerCloseTestPopupControlVM : IDialogContext
+{
+    public void Close()
+    {
+        RequestClose?.Invoke(this, Result);
+    }
+
+    public int Result { get; } = Random.Shared.Next();
+
+    public event EventHandler<object?>? RequestClose;
+}
