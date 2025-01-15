63be2d15ad4b965d8faf09dc6e937ec8458d797e
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-01-15 22:58:21 +0800

    fix: fix invisible issue when Dot=True & fix some style issues in Badge.

--- src/Ursa/Converters/BadgeContentOverflowConverter.cs
+++ src/Ursa/Converters/BadgeContentOverflowConverter.cs
@@ -3,18 +3,19 @@ using Avalonia.Data.Converters;
 
 namespace Ursa.Converters;
 
-public class BadgeContentOverflowConverter: IMultiValueConverter
+public class BadgeContentOverflowConverter : IMultiValueConverter
 {
     public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
     {
-        string overflowMark = parameter is string s ? s : "+";
-        if (double.TryParse(values[0]?.ToString(), out var b) && values[1] is int i and > 0)
+        var overflowMark = parameter as string ?? "+";
+        if (double.TryParse(values[0]?.ToString(), out var b) && values[1] is int count and > 0)
         {
-            if (b > i)
+            if (b > count)
             {
-                return i + overflowMark;
+                return $"{count}{overflowMark}";
             }
         }
+
         return values[0];
     }
 }
