a366ca6f0ca7b3637ccfb23fe814e0af3ae4bc77
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-01-17 16:05:52 +0800

    feat: using Padding instead of Gap in Avatar.

--- src/Ursa/Controls/Avatar/Avatar.cs
+++ src/Ursa/Controls/Avatar/Avatar.cs
@@ -7,20 +7,11 @@ namespace Ursa.Controls;
 
 public class Avatar : Button
 {
-    public static readonly StyledProperty<double> GapProperty = AvaloniaProperty.Register<Avatar, double>(
-        nameof(Gap));
-
     public static readonly StyledProperty<IImage?> SourceProperty = AvaloniaProperty.Register<Avatar, IImage?>(
         nameof(Source));
 
     public static readonly StyledProperty<object?> HoverMaskProperty = AvaloniaProperty.Register<Avatar, object?>(
         nameof(HoverMask));
-
-    public double Gap
-    {
-        get => GetValue(GapProperty);
-        set => SetValue(GapProperty, value);
-    }
 
     [ExcludeFromCodeCoverage]
     public IImage? Source
