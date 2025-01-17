8b3c95be593fc41b46bdafd0a2ffade3b1864004
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-01-18 01:46:15 +0800

    feat: enhance AvatarDemo.

--- demo/Ursa.Demo/ViewModels/AvatarDemoViewModel.cs
+++ demo/Ursa.Demo/ViewModels/AvatarDemoViewModel.cs
@@ -1,19 +1,56 @@
 ﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Avalonia.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Ursa.Controls;
 
 namespace Ursa.Demo.ViewModels;
 
 public partial class AvatarDemoViewModel : ViewModelBase
 {
     [ObservableProperty] private string _content = "AS";
-    [ObservableProperty] private bool _canClick = true;
 
-    [RelayCommand(CanExecute = nameof(CanClick))]
+    public ObservableCollection<string> Items => ["AS", "BM", "TJ", "ZL", "YZ"];
+
+    public ObservableCollection<Person> People =>
+    [
+        new()
+        {
+            Name = "AS",
+        },
+        new()
+        {
+            Name = "BM",
+        },
+        new()
+        {
+            Name = "TJ",
+        },
+        new()
+        {
+            Name = "ZL",
+        },
+        new()
+        {
+            Name = "YZ",
+        },
+    ];
+
+    [RelayCommand]
     private void Click()
     {
         Content = "BM";
     }
 
-    public ObservableCollection<string> Items => ["AS", "BM", "TJ", "ZL", "YZ"];
+    [RelayCommand]
+    private async Task Show(string name)
+    {
+        await MessageBox.ShowOverlayAsync($"Person '{name}' is clicked!");
+    }
+}
+
+public partial class Person : ObservableObject
+{
+    [ObservableProperty] private string? _name;
 }
