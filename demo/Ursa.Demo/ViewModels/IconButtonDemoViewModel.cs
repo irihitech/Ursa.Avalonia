841e0748f2d3ae3ea0fad3b9573fc5fc90eb1e0c
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-01-24 21:46:45 +0800

    feat: redesign IconButtonDemo.

--- demo/Ursa.Demo/ViewModels/IconButtonDemoViewModel.cs
+++ demo/Ursa.Demo/ViewModels/IconButtonDemoViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Ursa.Common;
 
 namespace Ursa.Demo.ViewModels;
 
-public class IconButtonDemoViewModel : ObservableObject
+public partial class IconButtonDemoViewModel : ObservableObject
 {
+    [ObservableProperty] private bool _isLoading;
+    [ObservableProperty] private Position _selectedPosition;
 }
