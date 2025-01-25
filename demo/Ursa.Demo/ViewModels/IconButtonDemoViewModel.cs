b97a7d26ae3386e28d557157c365f375392bc808
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-01-25 12:40:26 +0800

    feat: add Loading switch.

--- demo/Ursa.Demo/ViewModels/IconButtonDemoViewModel.cs
+++ demo/Ursa.Demo/ViewModels/IconButtonDemoViewModel.cs
@@ -6,5 +6,6 @@ namespace Ursa.Demo.ViewModels;
 public partial class IconButtonDemoViewModel : ObservableObject
 {
     [ObservableProperty] private bool _isLoading;
+    [ObservableProperty] private bool _isLoading2;
     [ObservableProperty] private Position _selectedPosition;
 }
