9e3f22c5577b7bc1827703159b87b5ab119c4154
Dong Bin <popmessiah@hotmail.com>
2025-04-15 22:10:44 +0800

    feat: fix various badge layout issues.

--- demo/Ursa.Demo/ViewModels/BadgeDemoViewModel.cs
+++ demo/Ursa.Demo/ViewModels/BadgeDemoViewModel.cs
@@ -1,6 +1,28 @@
+using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
 namespace Ursa.Demo.ViewModels;
 
-public class BadgeDemoViewModel: ViewModelBase
+public partial class BadgeDemoViewModel: ViewModelBase
 {
-    
+    [ObservableProperty] private string? _text = null;
+
+    public BadgeDemoViewModel()
+    {
+        
+    }
+
+    [RelayCommand]
+    public void ChangeText()
+    {
+        if (Text == null)
+        {
+            Text = DateTime.Now.ToShortDateString();
+        }
+        else
+        {
+            Text = null;
+        }
+    }
 }
