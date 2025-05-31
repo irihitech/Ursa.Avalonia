7f160489ff134b68c092ba9e15ce5fdd9fa04a8e
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-06-01 00:06:17 +0800

    fix: update ClosableTag behavior and styling.

--- src/Ursa/Controls/TagInput/ClosableTag.cs
+++ src/Ursa/Controls/TagInput/ClosableTag.cs
@@ -8,10 +8,11 @@ using Avalonia.Input;
 namespace Ursa.Controls;
 
 [TemplatePart(PART_CloseButton, typeof(PathIcon))]
-public class ClosableTag: ContentControl
+public class ClosableTag : ContentControl
 {
     public const string PART_CloseButton = "PART_CloseButton";
     private PathIcon? _icon;
+
     public static readonly StyledProperty<ICommand?> CommandProperty = AvaloniaProperty.Register<ClosableTag, ICommand?>(
         nameof(Command));
 
@@ -24,21 +25,21 @@ public class ClosableTag: ContentControl
     protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
     {
         base.OnApplyTemplate(e);
-        if (_icon != null)
+        if (_icon is not null)
         {
-            _icon.PointerPressed -= OnPointerPressed;
+            _icon.PointerReleased -= OnPointerReleased;
         }
+
         _icon = e.NameScope.Find<PathIcon>(PART_CloseButton);
-        if (_icon != null)
+        if (_icon is not null)
         {
-            _icon.PointerPressed += OnPointerPressed;
+            _icon.PointerReleased += OnPointerReleased;
         }
-        
     }
 
-    private void OnPointerPressed(object? sender, PointerPressedEventArgs args)
+    private void OnPointerReleased(object? sender, PointerReleasedEventArgs args)
     {
-        if (Command != null && Command.CanExecute(null))
+        if (Command is not null && Command.CanExecute(null))
         {
             Command.Execute(this);
         }
