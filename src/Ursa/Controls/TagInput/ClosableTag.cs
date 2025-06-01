d12188d8b3a14b78f613794a58cade3a54ce211c
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-06-02 02:22:26 +0800

    misc: remove unused close button field from ClosableTag.

--- src/Ursa/Controls/TagInput/ClosableTag.cs
+++ src/Ursa/Controls/TagInput/ClosableTag.cs
@@ -2,7 +2,6 @@ using System.Windows.Input;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Metadata;
-using Avalonia.Controls.Primitives;
 
 namespace Ursa.Controls;
 
@@ -10,7 +9,6 @@ namespace Ursa.Controls;
 public class ClosableTag : ContentControl
 {
     public const string PART_CloseButton = "PART_CloseButton";
-    private Button? _closeButton;
 
     public static readonly StyledProperty<ICommand?> CommandProperty = AvaloniaProperty.Register<ClosableTag, ICommand?>(
         nameof(Command));
@@ -19,11 +17,5 @@ public class ClosableTag : ContentControl
     {
         get => GetValue(CommandProperty);
         set => SetValue(CommandProperty, value);
-    }
-
-    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
-    {
-        base.OnApplyTemplate(e);
-        _closeButton = e.NameScope.Find<Button>(PART_CloseButton);
     }
 }
