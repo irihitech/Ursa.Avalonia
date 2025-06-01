b81d6c69a773d41924646827bf03a6ae299eba3a
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-06-02 02:12:24 +0800

    feat: replace PathIcon with Button.

--- src/Ursa/Controls/TagInput/ClosableTag.cs
+++ src/Ursa/Controls/TagInput/ClosableTag.cs
@@ -3,15 +3,14 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Metadata;
 using Avalonia.Controls.Primitives;
-using Avalonia.Input;
 
 namespace Ursa.Controls;
 
-[TemplatePart(PART_CloseButton, typeof(PathIcon))]
+[TemplatePart(PART_CloseButton, typeof(Button))]
 public class ClosableTag : ContentControl
 {
     public const string PART_CloseButton = "PART_CloseButton";
-    private PathIcon? _icon;
+    private Button? _closeButton;
 
     public static readonly StyledProperty<ICommand?> CommandProperty = AvaloniaProperty.Register<ClosableTag, ICommand?>(
         nameof(Command));
@@ -25,23 +24,6 @@ public class ClosableTag : ContentControl
     protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
     {
         base.OnApplyTemplate(e);
-        if (_icon is not null)
-        {
-            _icon.PointerReleased -= OnPointerReleased;
-        }
-
-        _icon = e.NameScope.Find<PathIcon>(PART_CloseButton);
-        if (_icon is not null)
-        {
-            _icon.PointerReleased += OnPointerReleased;
-        }
-    }
-
-    private void OnPointerReleased(object? sender, PointerReleasedEventArgs args)
-    {
-        if (Command is not null && Command.CanExecute(null))
-        {
-            Command.Execute(this);
-        }
+        _closeButton = e.NameScope.Find<Button>(PART_CloseButton);
     }
 }
