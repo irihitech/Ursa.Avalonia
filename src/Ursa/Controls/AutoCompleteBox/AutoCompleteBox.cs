182eb28da5548b017db6033c405a20df80c40e45
Dong Bin <popmessiah@hotmail.com>
2025-06-07 18:29:04 +0800

    refactor: improve AutoCompleteBox behavior and formatting

--- src/Ursa/Controls/AutoCompleteBox/AutoCompleteBox.cs
+++ src/Ursa/Controls/AutoCompleteBox/AutoCompleteBox.cs
@@ -1,14 +1,11 @@
-using System.Diagnostics;
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
 using Avalonia.Input;
 using Avalonia.Interactivity;
 using Irihi.Avalonia.Shared.Contracts;
 
 namespace Ursa.Controls;
 
-public class AutoCompleteBox: Avalonia.Controls.AutoCompleteBox, IClearControl
+public class AutoCompleteBox : Avalonia.Controls.AutoCompleteBox, IClearControl
 {
     static AutoCompleteBox()
     {
@@ -19,24 +16,24 @@ public class AutoCompleteBox: Avalonia.Controls.AutoCompleteBox, IClearControl
     {
         AddHandler(PointerPressedEvent, OnBoxPointerPressed, RoutingStrategies.Tunnel);
     }
-    
+
+    public void Clear()
+    {
+        SetCurrentValue(SelectedItemProperty, null);
+    }
+
     private void OnBoxPointerPressed(object? sender, PointerPressedEventArgs e)
     {
-        if (Equals(sender, this) && e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
-        {
+        if (Equals(sender, this) && e.GetCurrentPoint(this).Properties.IsLeftButtonPressed && IsDropDownOpen == false)
             SetCurrentValue(IsDropDownOpenProperty, true);
-        }
     }
 
     protected override void OnGotFocus(GotFocusEventArgs e)
     {
         base.OnGotFocus(e);
-        if (IsDropDownOpen) return;
-        SetCurrentValue(IsDropDownOpenProperty, true);
-    }
-
-    public void Clear()
-    {
-        SetCurrentValue(SelectedItemProperty, null);
+        if (e.NavigationMethod != NavigationMethod.Directional && e.NavigationMethod != NavigationMethod.Tab) return;
+        if (!this.GetTemplateChildren().Contains(e.Source)) return;
+        // If the focus is set by keyboard navigation, open the dropdown.
+        if (IsDropDownOpen == false) SetCurrentValue(IsDropDownOpenProperty, true);
     }
 }
