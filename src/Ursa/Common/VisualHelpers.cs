0323a873d5902a0c1803f304c5799cc8e7ffab6d
Dong Bin <popmessiah@hotmail.com>
2025-07-08 15:18:41 +0800

    feat: add generic constraint to fix null reference issue.

--- src/Ursa/Common/VisualHelpers.cs
+++ src/Ursa/Common/VisualHelpers.cs
@@ -1,13 +1,14 @@
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.VisualTree;
 
 namespace Ursa.Common;
 
 public static class VisualHelpers
 {
-    public static T? GetContainerFromEventSource<T>(this Visual? source)
+    public static T? GetContainerFromEventSource<T>(this Visual? source) where T: Control
     {
-        var item = source.GetSelfAndVisualAncestors().OfType<T>().FirstOrDefault();
+        var item = source?.GetSelfAndVisualAncestors().OfType<T>().FirstOrDefault();
         return item;
     }
 }
