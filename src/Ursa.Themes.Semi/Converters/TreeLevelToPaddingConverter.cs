3ae76efd45c6092d6a918638ff11f70b8c000598
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2025-07-10 19:55:34 +0800

    feat: update AnchorItem Padding Converter.

--- src/Ursa.Themes.Semi/Converters/TreeLevelToPaddingConverter.cs
+++ src/Ursa.Themes.Semi/Converters/TreeLevelToPaddingConverter.cs
@@ -4,14 +4,15 @@ using Avalonia.Data.Converters;
 
 namespace Ursa.Themes.Semi.Converters;
 
-public class TreeLevelToMarginConverter: IMultiValueConverter
+public class TreeLevelToPaddingConverter : IMultiValueConverter
 {
     public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (values[0] is int i && values[1] is double indent)
+        if (values[0] is int i && values[1] is Thickness indent)
         {
-            return new Thickness(Math.Max(i-1, 0) * indent, 0, 0, 0);
+            return new Thickness(Math.Max(i, 0) * indent.Left, indent.Top, indent.Right, indent.Bottom);
         }
+
         return new Thickness();
     }
 }
