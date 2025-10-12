4a2df16f55ecdc2873bd9f278c2a4f5cd0de354c
rabbitism <dongbin@irihi.tech>
2025-10-12 16:49:28 +0800

    feat: update demo.

--- demo/Ursa.Demo/ViewModels/DescriptionsDemoViewModel.cs
+++ demo/Ursa.Demo/ViewModels/DescriptionsDemoViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using Avalonia.Controls;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Ursa.Demo.ViewModels;
@@ -6,6 +7,7 @@ namespace Ursa.Demo.ViewModels;
 public partial class DescriptionsDemoViewModel: ObservableObject
 {
     public ObservableCollection<DescriptionItemViewModel> Items { get; set; }
+    public ObservableCollection<DescriptionItemViewModel> Items2 { get; set; }
 
     public DescriptionsDemoViewModel()
     {
@@ -26,6 +28,16 @@ public partial class DescriptionsDemoViewModel: ObservableObject
                 Label = "Security Level",
                 Description = "III"
             }
+        };
+        Items2 = new ObservableCollection<DescriptionItemViewModel>()
+        {
+            new() { Label = "抖音号", Description = "SemiDesign" },
+            new() { Label = "主播类型", Description = "自由主播" },
+            new() { Label = "安全等级", Description = "3级" },
+            new() { Label = "垂类标签", Description = "编程" },
+            new() { Label = "作品数量", Description = "88888888" },
+            new() { Label = "认证状态", Description = "这是一个很长很长很长很长很长很长很长很长很长的值" },
+            new() { Label = "上次直播时间", Description = "2024-05-01 12:00:00"}
         };
     }
 }
