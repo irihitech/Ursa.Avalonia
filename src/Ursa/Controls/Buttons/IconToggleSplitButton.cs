af3e531f85384de5732b97ad75a618e58844a968
Athari <athari.p@gmail.com>
2025-11-23 07:57:58 +0300

    Added IconToggleSplitButton (#812) * Fixed tab order in IconSplitButton and IconToggleSplitButton (DockPanel messes up tab order, TabIndex is global and makes it even worse, so just switched to Grid).

---
+++ src/Ursa/Controls/Buttons/IconToggleSplitButton.cs
@@ -0,0 +1,54 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Ursa.Common;
+
+namespace Ursa.Controls;
+
+public class IconToggleSplitButton : ToggleSplitButton, IIconButton
+{
+    public static readonly StyledProperty<object?> IconProperty =
+        IconButton.IconProperty.AddOwner<IconRepeatButton>();
+
+    public object? Icon
+    {
+        get => GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+
+    public static readonly StyledProperty<IDataTemplate?> IconTemplateProperty =
+        IconButton.IconTemplateProperty.AddOwner<IconRepeatButton>();
+
+    public IDataTemplate? IconTemplate
+    {
+        get => GetValue(IconTemplateProperty);
+        set => SetValue(IconTemplateProperty, value);
+    }
+
+    public static readonly StyledProperty<bool> IsLoadingProperty =
+        IconButton.IsLoadingProperty.AddOwner<IconRepeatButton>();
+
+    public bool IsLoading
+    {
+        get => GetValue(IsLoadingProperty);
+        set => SetValue(IsLoadingProperty, value);
+    }
+
+    public static readonly StyledProperty<Position> IconPlacementProperty =
+        AvaloniaProperty.Register<IconButton, Position>(nameof(IconPlacement), defaultValue: Position.Left);
+
+    public Position IconPlacement
+    {
+        get => GetValue(IconPlacementProperty);
+        set => SetValue(IconPlacementProperty, value);
+    }
+
+    IPseudoClasses IIconButton.PseudoClasses => PseudoClasses;
+
+    static IconToggleSplitButton()
+    {
+        ReversibleStackPanelUtils.EnsureBugFixed();
+    }
+
+    protected override Type StyleKeyOverride => GetType();
+}
