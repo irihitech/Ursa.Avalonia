691b08fdbd118fc545f6710dc6205fb9823e41ad
Dong Bin <popmessiah@hotmail.com>
2026-07-22 02:00:14 +0800

    fix: update MarkdownLine demo ViewModel to match new PageMetadata structure

--- demo/Ursa.Demo/ViewModels/Pages/MarkdownLineDemoViewModel.cs
+++ demo/Ursa.Demo/ViewModels/Pages/MarkdownLineDemoViewModel.cs
@@ -1,14 +1,15 @@
+using Ursa.Demo.Localizations;
 using Ursa.Demo.ViewModels.Controls;
 
 namespace Ursa.Demo.ViewModels;
 
-public partial class MarkdownLineDemoViewModel : ViewModelBase
+public partial class MarkdownLineDemoViewModel : ViewModelBase, IPageMetadataProvider
 {
     public PageMetadataViewModel PageMetadata { get; set; } = new()
     {
-        Title = "MarkdownLine",
-        Description = "A lightweight TextBlock that renders a limited subset of inline Markdown formatting.",
-        Breadcrumbs = ["Layout & Display", "MarkdownLine"],
+        Title = LanguageManager.Instance.Page_Title_MarkdownLine,
+        Description = LanguageManager.Instance.Page_Description_MarkdownLine,
+        Breadcrumbs = [new BreadcrumbItemData(LanguageManager.Instance.Menu_Category_LayoutAndDisplay), new BreadcrumbItemData(LanguageManager.Instance.Menu_Header_MarkdownLine)],
         Tags = ["Markdown", "Text", "Inline", "Formatting"],
         DemoViewUrl = "https://github.com/irihitech/Ursa.Avalonia/blob/main/demo/Ursa.Demo/Pages/MarkdownLineDemo.axaml",
         DemoViewModelUrl = "https://github.com/irihitech/Ursa.Avalonia/blob/main/demo/Ursa.Demo/ViewModels/Pages/MarkdownLineDemoViewModel.cs",
