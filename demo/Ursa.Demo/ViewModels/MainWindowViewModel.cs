78993dd0d0e321e32ea366398b4735a6025e8efb
Dong Bin <popmessiah@hotmail.com>
2026-07-21 15:57:09 +0800

    refactor: Update layout and bindings in MainView and PageMetadataView

--- demo/Ursa.Demo/ViewModels/MainWindowViewModel.cs
+++ demo/Ursa.Demo/ViewModels/MainWindowViewModel.cs
@@ -6,10 +6,5 @@ namespace Ursa.Demo.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public List<BreadcrumbItemData> BreadcrumbItems { get; set; } = new List<BreadcrumbItemData>
-    {
-        new BreadcrumbItemData(LanguageManager.Instance.Menu_Category_ButtonsAndInputs),
-        new BreadcrumbItemData(LanguageManager.Instance.Menu_Header_IconButton),
-    };
     public MainViewViewModel MainViewViewModel { get; set; } = new MainViewViewModel();
 }
