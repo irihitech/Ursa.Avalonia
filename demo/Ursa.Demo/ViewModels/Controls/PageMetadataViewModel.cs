9f657012836a98007000dba984333cab245e191a
Dong Bin <popmessiah@hotmail.com>
2026-07-21 14:58:32 +0800

    refactor: PageMetadataViewModel 三个属性改回 [ObservableProperty] 字段风格
    移除手动实现的 SetProperty，统一使用 source generator 生成，与类中其他属性风格一致

--- demo/Ursa.Demo/ViewModels/Controls/PageMetadataViewModel.cs
+++ demo/Ursa.Demo/ViewModels/Controls/PageMetadataViewModel.cs
@@ -6,28 +6,9 @@ namespace Ursa.Demo.ViewModels.Controls;
 
 public partial class PageMetadataViewModel: ObservableObject
 {
-    private IObservable<string?>? _title;
-    private IObservable<string?>? _description;
-    private IReadOnlyList<BreadcrumbItemData>? _breadcrumbs;
-
-    public IObservable<string?>? Title
-    {
-        get => _title;
-        set => SetProperty(ref _title, value);
-    }
-
-    public IObservable<string?>? Description
-    {
-        get => _description;
-        set => SetProperty(ref _description, value);
-    }
-
-    public IReadOnlyList<BreadcrumbItemData>? Breadcrumbs
-    {
-        get => _breadcrumbs;
-        set => SetProperty(ref _breadcrumbs, value);
-    }
-
+    [ObservableProperty] private IObservable<string?>? _title;
+    [ObservableProperty] private IObservable<string?>? _description;
+    [ObservableProperty] private IReadOnlyList<BreadcrumbItemData>? _breadcrumbs;
     [ObservableProperty] private string? _demoViewUrl;
     [ObservableProperty] private string? _demoViewModelUrl;
     [ObservableProperty] private string? _sourceUrl;
